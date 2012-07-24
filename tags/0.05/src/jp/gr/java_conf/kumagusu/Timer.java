eff6651cc259234ab52838d3715af35b592b9903
tarshi <tarshi@d3225274-db70-448d-83ab-cbe94b491076>
2012-07-24 09:04:48 +0000

    バージョン0.05(2)

---
+++ tags/0.05/src/jp/gr/java_conf/kumagusu/Timer.java
@@ -0,0 +1,101 @@
+package jp.gr.java_conf.kumagusu;
+
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * タイマー.
+ *
+ * @author tarshi
+ */
+public final class Timer
+{
+    /**
+     * コンテキスト.
+     */
+    private Context context;
+
+    /**
+     * 開始時間.
+     */
+    private long startDateTime;
+
+    /**
+     * タイマーを初期化する.
+     *
+     * @param con コンテキスト
+     */
+    public Timer(Context con)
+    {
+        this.context = con;
+    }
+
+    /**
+     * タイマーを開始する.
+     */
+    public void start()
+    {
+        Log.d("AutoCloseTimer", "*** START start()");
+
+        long delayTime = MainPreferenceActivity.getAutocloseDelaytime(this.context);
+
+        this.startDateTime = 0;
+
+        if (delayTime >= 0)
+        {
+            this.startDateTime = new Date().getTime();
+        }
+    }
+
+    /**
+     * タイマーを停止する.
+     *
+     * @return trueのときタイムアウト発生
+     */
+    public boolean stop()
+    {
+        Log.d("AutoCloseTimer", "*** START stop()");
+
+        long delayTime = MainPreferenceActivity.getAutocloseDelaytime(this.context);
+
+        boolean ret = false;
+
+        if ((delayTime >= 0) && (this.startDateTime != 0))
+        {
+            if ((new Date().getTime() - this.startDateTime) >= delayTime)
+            {
+                // タイムアウト！
+                ret = true;
+            }
+        }
+
+        this.startDateTime = 0;
+
+        return ret;
+    }
+
+    /**
+     * 自動クローズ処理.
+     *
+     * @author tarshi
+     */
+    class AutoClosure implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            // 一応タイマを破棄
+            stop();
+
+            // パスワードをクリア
+            MainApplication.getInstance((Activity) context).clearPasswordList();
+
+            // アプリケーションを終了する
+            Activity act = (Activity) context;
+            act.moveTaskToBack(true);
+        }
+    }
+}
