96e4e0022c71b42beaf51a042438ee8d5cc89fb8
tarshi <tarshi@d3225274-db70-448d-83ab-cbe94b491076>
2012-08-06 09:47:00 +0000

    自動リンクを再実装中。

---
+++ src/jp/gr/java_conf/kumagusu/AutoLinkClickableSpan.java
@@ -0,0 +1,82 @@
+package jp.gr.java_conf.kumagusu;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 自動リンク用のSpan処理.
+ *
+ * @author tarshi
+ *
+ */
+public final class AutoLinkClickableSpan extends ClickableSpan
+{
+    /**
+     * 自動リンクパターン.
+     */
+    private Pattern pattern;
+
+    /**
+     * 自動リンクのクリックイベントリスナ.
+     */
+    private AutoLinkOnClickListener onClickListener;
+
+    /**
+     * コンストラクタ.
+     *
+     * @param patt 自動リンクパターン
+     * @param listener 自動リンクのクリックイベントリスナ
+     */
+    public AutoLinkClickableSpan(Pattern patt, AutoLinkOnClickListener listener)
+    {
+        this.pattern = patt;
+        this.onClickListener = listener;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if (!(view instanceof TextView))
+        {
+            return;
+        }
+
+        TextView textView = (TextView) view;
+
+        int selStart = textView.getSelectionStart();
+        int selEnd = textView.getSelectionEnd();
+        CharSequence text = textView.getText();
+        String selString = text.subSequence(selStart, selEnd).toString();
+
+        Matcher matcher = pattern.matcher(selString);
+
+        if (matcher.find())
+        {
+            if (this.onClickListener != null)
+            {
+                this.onClickListener.onClick(matcher.group());
+            }
+        }
+    }
+
+    /**
+     * 自動リンクのクリックイベントリスナ.
+     *
+     * @author tarshi
+     *
+     */
+    public interface AutoLinkOnClickListener
+    {
+        /**
+         * 自動リンクのクリックイベントを処理する.
+         *
+         * @param matchString リンク文字列
+         */
+        void onClick(String matchString);
+    }
+}
+
