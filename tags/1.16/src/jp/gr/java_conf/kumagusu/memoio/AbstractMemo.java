b6f3b6c82a53ce51f2adc9095a4bb7e4f5629219
tarshi <tarshi@d3225274-db70-448d-83ab-cbe94b491076>
2016-11-05 04:41:17 +0000

    バージョン1.16

---
+++ tags/1.16/src/jp/gr/java_conf/kumagusu/memoio/AbstractMemo.java
@@ -0,0 +1,174 @@
+package jp.gr.java_conf.kumagusu.memoio;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import android.content.Context;
+
+/**
+ * メモ.
+ *
+ * @author tarshi
+ */
+abstract class AbstractMemo implements IMemo
+{
+    /**
+     * メモの保存フォルダオブジェクト.
+     */
+    private File folderFile;
+
+    /**
+     * エンコーディング名.
+     */
+    private String encodingName;
+
+    /**
+     * メモ内容とタイトルをリンクするか？.
+     */
+    private boolean titleLinkFg;
+
+    /**
+     * メモのFileオブジェクト.
+     */
+    private File memoFile;
+
+    /**
+     * メモ種別.
+     */
+    private MemoType memoType;
+
+    /**
+     * コンテキスト.
+     */
+    private Context context;
+
+    /**
+     * 既存ファイルを使用する場合（表示時）のコンストラクタ.
+     *
+     * @param con コンテキスト
+     * @param mFileOrDir メモまたはメモフォルダのFileオブジェクト
+     * @param encName エンコーディング名
+     * @param tlkFg ファイルの一行目とタイトルを連動するか
+     * @param type メモ種別
+     * @throws FileNotFoundException
+     */
+    AbstractMemo(Context con, File mFileOrDir, String encName, boolean tlkFg, MemoType type)
+            throws FileNotFoundException
+    {
+        this.context = con;
+        this.encodingName = encName;
+        this.titleLinkFg = tlkFg;
+        this.memoType = type;
+
+        if (mFileOrDir.isFile()) // メモ
+        {
+            this.memoFile = mFileOrDir;
+            this.folderFile = this.memoFile.getParentFile();
+        }
+        else if (mFileOrDir.isDirectory()) // フォルダ
+        {
+            this.memoFile = null;
+            this.folderFile = mFileOrDir;
+        }
+        else
+        {
+            throw new FileNotFoundException("not file or directory.");
+        }
+    }
+
+    @Override
+    public MemoType getMemoType()
+    {
+        return this.memoType;
+    }
+
+    /**
+     * メモの保存フォルダオブジェクトを返す.
+     *
+     * @return メモの保存フォルダオブジェクト
+     */
+    protected File getFolderFile()
+    {
+        return this.folderFile;
+    }
+
+    /**
+     * エンコーディング名を返す.
+     *
+     * @return エンコーディング名
+     */
+    protected String getEncodingName()
+    {
+        return this.encodingName;
+    }
+
+    /**
+     * メモ内容とタイトルをリンクするか?
+     *
+     * @return trueのときメモ内容とタイトルをリンクする
+     */
+    protected boolean isTitleLinkFg()
+    {
+        return this.titleLinkFg;
+    }
+
+    /**
+     * メモのFileオブジェクトを返す.
+     *
+     * @return メモのFileオブジェクト
+     */
+    protected File getMemoFile()
+    {
+        return this.memoFile;
+    }
+
+    /**
+     * メモのFileオブジェクトを設定する.
+     *
+     * @param file メモのFileオブジェクト
+     */
+    protected void setMemoFile(File file)
+    {
+        this.memoFile = file;
+    }
+
+    /**
+     * コンテキストを返す.
+     *
+     * @return コンテキスト
+     */
+    protected Context getContext()
+    {
+        return this.context;
+    }
+
+    /**
+     * フォルダを変更する。
+     *
+     * @param folder フォルダ
+     * @return 成功した場合true
+     */
+    public boolean setParentFolder(String folder)
+    {
+        File newFolder = new File(folder);
+
+        if (newFolder.isDirectory())
+        {
+            if (memoFile != null)
+            {
+                this.memoFile = new File(newFolder, this.memoFile.getName());
+                this.folderFile = newFolder;
+            }
+            else
+            {
+                this.folderFile = newFolder;
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
